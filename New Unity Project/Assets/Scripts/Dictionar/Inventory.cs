9bd97185618967db598a4e89874fa26422ebaf7b
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-03-27 19:51:52 +0100

    YoOT

--- New Unity Project/Assets/Scripts/Dictionar/Inventory.cs
+++ New Unity Project/Assets/Scripts/Dictionar/Inventory.cs
@@ -4,50 +4,68 @@ using UnityEngine;
 
 //opdracht 3 - inventory system, tracking 
 public class Inventory : MonoBehaviour
-{    
-    public enum keys
+{
+    public enum Keys
     {
         MACHINE_GUN,
         PISTOL,
         THERMONUCLEAR_WARHEAD
     }
 
-    private keys curWep;
+    private Keys curWep;
 
-    private Dictionary<keys, List<GameObject>> inventory = new Dictionary<keys, List<GameObject>>();
+    private Dictionary<Keys, List<GameObject>> inventory = new Dictionary<Keys, List<GameObject>>();
+
 
     private void Start()
     {
-        curWep = keys.MACHINE_GUN;
-        inventory[keys.MACHINE_GUN] = new List<GameObject>();
-        inventory[keys.PISTOL] = new List<GameObject>();
-        inventory[keys.THERMONUCLEAR_WARHEAD] = new List<GameObject>();
+        curWep = Keys.MACHINE_GUN;
+        inventory[Keys.MACHINE_GUN] = new List<GameObject>();
+        inventory[Keys.PISTOL] = new List<GameObject>();
+        inventory[Keys.THERMONUCLEAR_WARHEAD] = new List<GameObject>();
     }
 
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if(Input.GetKeyDown(KeyCode.W))
         {
-            
+            input();
         }
         if (Input.GetKeyDown(KeyCode.P))
         {
             PrintList();
         }
-        if (Input.GetKeyDown(KeyCode.W))
+    }
+
+    private void input()
+    {
+        if (inventory.ContainsKey(Keys.MACHINE_GUN))
         {
-            
+
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        //int temp_int;ddd
+        //temp_int = (int)curWep;
+        switch ((int)curWep)
         {
-            
+            case 0:
+                print("Machine gun");
+                break;
+
+            case 1:
+                print("Pistol");
+                break;
+
+            case 2:
+                print("Thermonuclear weapon");
+                break;
         }
+
     }
 
     public void SetMachineGunAmmo(GameObject gameobject)
     {
-        inventory[keys.MACHINE_GUN].Add(gameObject);
+        inventory[Keys.MACHINE_GUN].Add(gameObject);
     }
 
     private void PrintList()
